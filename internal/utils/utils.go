48eb87c9e6c4beee76fe2d59f6d9cbfef810d2b0
Eric Zorn <zornwebdev@gmail.com>
2025-06-20 11:43:14 -0400

    fix: update referer

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -16,7 +16,7 @@ import (
 // It uses a custom HTTP client that forces HTTP/1.1 to avoid issues with HTTP/2.
 func FindCyberTrucks() (models.CyberTruckInventoryResponse, error) {
 	// The URL for the Tesla inventory API.
-	url := "https://www.tesla.com/inventory/api/v4/inventory-results?query=%7B%22query%22%3A%7B%22model%22%3A%22ct%22%2C%22condition%22%3A%22new%22%2C%22options%22%3A%7B%22TRIM%22%3A%5B%22CTAWD%22%5D%2C%22WHEELS%22%3A%5B%22CYBER_WHEELS%22%5D%2C%22INTERIOR%22%3A%5B%22GREY%22%5D%7D%2C%22arrangeby%22%3Anull%2C%22order%22%3Anull%2C%22market%22%3A%22US%22%2C%22language%22%3A%22en%22%2C%22super_region%22%3A%22north%20america%22%2C%22PaymentType%22%3A%22lease%22%2C%22paymentRange%22%3A1800%2C%22lng%22%3A-73.99475629999999%2C%22lat%22%3A41.0229118%2C%22zip%22%3A%2207675%22%2C%22range%22%3A100%2C%22region%22%3A%22NJ%22%7D%2C%22offset%22%3A0%2C%22count%22%3A24%2C%22outsideOffset%22%3A0%2C%22outsideSearch%22%3Afalse%2C%22isFalconDeliverySelectionEnabled%22%3Atrue%2C%22version%22%3A%22v2%22%7D"
+	const url string = "https://www.tesla.com/inventory/api/v4/inventory-results?query=%7B%22query%22%3A%7B%22model%22%3A%22ct%22%2C%22condition%22%3A%22new%22%2C%22options%22%3A%7B%22TRIM%22%3A%5B%22CTAWD%22%5D%2C%22WHEELS%22%3A%5B%22CYBER_WHEELS%22%5D%2C%22INTERIOR%22%3A%5B%22GREY%22%5D%7D%2C%22arrangeby%22%3Anull%2C%22order%22%3Anull%2C%22market%22%3A%22US%22%2C%22language%22%3A%22en%22%2C%22super_region%22%3A%22north%20america%22%2C%22PaymentType%22%3A%22lease%22%2C%22paymentRange%22%3A1800%2C%22lng%22%3A-73.99475629999999%2C%22lat%22%3A41.0229118%2C%22zip%22%3A%2207675%22%2C%22range%22%3A100%2C%22region%22%3A%22NJ%22%7D%2C%22offset%22%3A0%2C%22count%22%3A24%2C%22outsideOffset%22%3A0%2C%22outsideSearch%22%3Afalse%2C%22isFalconDeliverySelectionEnabled%22%3Atrue%2C%22version%22%3A%22v2%22%7D"
 
 	// Create a custom HTTP transport.
 	// Setting ForceAttemptHTTP2 to false explicitly disables HTTP/2 for this transport.
@@ -36,13 +36,13 @@ func FindCyberTrucks() (models.CyberTruckInventoryResponse, error) {
 	}
 
 	// Add standard headers to make the request appear more like a browser's.
-	req.Header.Set("User-Agent", "PostmanRuntime/7.44.0")  // Set a common User-Agent.
-	req.Header.Set("Accept", "application/json")           // Request JSON content specifically.
-	req.Header.Set("Accept", "*/*")                        // Accept all content types.
-	req.Header.Set("Referer", "https://www.tesla.com/")    // Set a referer to mimic browser behavior.
-	req.Header.Set("Connection", "keep-alive")             // Use keep-alive to maintain the connection.
-	req.Header.Set("Cache-Control", "no-cache")            // Disable caching to ensure fresh data.
-	req.Header.Set("Accept-Encoding", "gzip, deflate, br") // Accept compressed responses.
+	req.Header.Set("User-Agent", "PostmanRuntime/7.44.0")                                                                                                                    // Set a common User-Agent.
+	req.Header.Set("Accept", "application/json")                                                                                                                             // Request JSON content specifically.
+	req.Header.Set("Accept", "*/*")                                                                                                                                          // Accept all content types.
+	req.Header.Set("Referer", "https://www.tesla.com/inventory/new/ct?TRIM=CTAWD&WHEELS=CYBER_WHEELS&INTERIOR=GREY&arrangeby=savings&zip=07675&range=100&PaymentType=lease") // Set a referer to mimic browser behavior.
+	req.Header.Set("Connection", "keep-alive")                                                                                                                               // Use keep-alive to maintain the connection.
+	req.Header.Set("Cache-Control", "no-cache")                                                                                                                              // Disable caching to ensure fresh data.
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")                                                                                                                   // Accept compressed responses.
 
 	logger.Logger.Info("Fetching Tesla Cybertruck inventory data (forcing HTTP/1.1)")
 
